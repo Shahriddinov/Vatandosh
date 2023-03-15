d0276978a5a2139198c1d225ae61ec4e7499c91d
Shahriddinov <shahriddinovmurod195@gmail.com>
2023-03-15 11:04:34 +0500

    header layout

--- src/component/Layout/Header/component/Menu/Menu.jsx
+++ src/component/Layout/Header/component/Menu/Menu.jsx
@@ -1,68 +1,101 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, {useEffect, useRef, useState} from "react";
 import "./menu.scss";
-import { motion } from "framer-motion";
-import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
+import {motion} from "framer-motion";
+import {useTranslation} from "react-i18next";
+import {Link} from "react-router-dom";
 
 const Menu = () => {
-  const navbarRef = useRef();
-  const scrollRef = useRef(null);
-  const [isFixed, setFixed] = useState(false);
-  const { t } = useTranslation();
+    const navbarRef = useRef();
+    const scrollRef = useRef(null);
+    const [isFixed, setFixed] = useState(false);
+    const {t} = useTranslation();
 
-  useEffect(() => {
-    scrollRef.current = document.querySelector(".navbar").offsetTop + 18;
-  }, []);
+    useEffect(() => {
+        scrollRef.current = document.querySelector(".navbar").offsetTop + 18;
+    }, []);
 
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.pageYOffset >= 92) {
-        setFixed(true);
-      } else {
-        setFixed(false);
-      }
-    });
-  }, [scrollRef.current]);
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.pageYOffset >= 92) {
+                setFixed(true);
+            } else {
+                setFixed(false);
+            }
+        });
+    }, [scrollRef.current]);
 
-  return (
-    <div className={`navbar ${isFixed ? "fixed" : ""}`} ref={navbarRef}>
-      <ul className="menu">
-        <motion.li whileTap={{ scale: 0.6 }} className="menu_item">
-          <Link to="/about" className="menu_link">
-            {t("about")}
-          </Link>
-        </motion.li>
-        <motion.li whileTap={{ scale: 0.6 }} className="menu_item">
-          <Link to="/" className="menu_link">
-            {t("citizin")}
-          </Link>
-        </motion.li>
-        <motion.li whileTap={{ scale: 0.6 }} className="menu_item">
-          <Link to="/" className="menu_link">
-            {t("projects")}
-          </Link>
-        </motion.li>
-        <motion.li whileTap={{ scale: 0.6 }} className="menu_item">
-          <Link to="/" className="menu_link">
-            {t("contects")}
-          </Link>
-        </motion.li>
-        <motion.li whileTap={{ scale: 0.6 }} className="menu_item">
-          <Link to="/" className="menu_link">
-            {t("information")}
-          </Link>
-        </motion.li>
-        <motion.li whileTap={{ scale: 0.6 }} className="menu_item">
-          <Link to="/" className="menu_link">
-            {t("link")}
-          </Link>
-        </motion.li>
-        <motion.li whileTap={{ scale: 0.6 }} className="menu_item">
-          ...
-        </motion.li>
-      </ul>
-    </div>
-  );
+    return (
+        <div className={`navbar ${isFixed ? "fixed" : ""}`} ref={navbarRef}>
+            <ul className="menu">
+
+
+                <motion.li whileTap={{scale: 0.6}} className="menu_item hov">
+                    <Link to="/about" className="menu_link">
+                        {t("about")}
+                    </Link>
+                    <ul className="main">
+                        <li>Biz haqimizda</li>
+                        <li>Vasiylik Kengashi</li>
+
+                    </ul>
+
+
+                </motion.li>
+                <motion.li whileTap={{scale: 0.6}} className="menu_item hov">
+                    <Link to="/" className="menu_link">
+                        {t("citizin")}
+                    </Link>
+                    <ul className="main">
+                        <li>Jamoat birlashmalar</li>
+                        <li>Turkum ko'rsatuvlar</li>
+                        <li>Jamoat birlashmalar tadbirlari</li>
+
+                    </ul>
+                </motion.li>
+                <motion.li whileTap={{scale: 0.6}} className="menu_item hov">
+                    <Link to="/" className="menu_link">
+                        {t("projects")}
+                    </Link>
+                    <ul className="main">
+                        <li>Yangiliklar</li>
+                        <li>Tadbirlar</li>
+                        <li>Mediateka</li>
+                        <li>Infografika</li>
+                        <li>Vatandoshlar jurnali</li>
+
+                    </ul>
+                </motion.li>
+                <motion.li whileTap={{scale: 0.6}} className="menu_item hov">
+                    <Link to="/" className="menu_link">
+                        {t("contects")}
+                    </Link>
+                    <ul className="main">
+                        <li>Barcha loyihalar</li>
+                        <li>“Kun oilasi” rukni</li>
+                        <li>“Sportchi vatandsohlar” rukni</li>
+                        <li>“Sportchi vatandsohlar” rukni</li>
+                        <li>Sara durdona</li>
+                        <li>“Xorijdagi milliy oshxona” loyihasi</li>
+                        <li>“Xorijdagi vatandosh tashkilot” loyihasi</li>
+
+                    </ul>
+                </motion.li>
+                <motion.li whileTap={{scale: 0.6}} className="menu_item">
+                    <Link to="/" className="menu_link">
+                        {t("information")}
+                    </Link>
+                </motion.li>
+                <motion.li whileTap={{scale: 0.6}} className="menu_item">
+                    <Link to="/" className="menu_link">
+                        {t("link")}
+                    </Link>
+                </motion.li>
+                <motion.li whileTap={{scale: 0.6}} className="menu_item">
+                    ...
+                </motion.li>
+            </ul>
+        </div>
+    );
 };
 
 export default Menu
