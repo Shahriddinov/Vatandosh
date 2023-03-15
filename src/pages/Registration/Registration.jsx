ba023bdba2ccf193f5ae47ff0385e1b8f5c3c083
Jasur <76224872+JasurNurgiljonov@users.noreply.github.com>
2023-03-15 13:18:18 +0500

    Minor changes

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { LoginIn } from './component/LoginIn/LoginIn'
 import { LoginUp } from './component/LoginUp/LoginUp'
+import Register from './component/Register/Register'
 
 export const Registration = () => {
     return (
         <>
-            <LoginIn />
-            <LoginUp />
+            <Register />
         </>
     )
 }
