9c7734747482ed4f0c9496410e30a2a60e7086e4
khushnudeshtemirov <khushnudeshtemirov@gmail.com>
2023-03-17 01:34:52 +0500

    Fixed

--- src/component/Layout/Layout.jsx
+++ src/component/Layout/Layout.jsx
@@ -10,58 +10,72 @@ import RegisterFooter from "../../pages/Registration/pages/Layout/Footer/Registe
 export const GrayContext = createContext();
 
 const Layout = (props) => {
-    const { pathname } = useLocation();
-    const { children } = props;
-    const [text, setText] = useState("");
-    const [speaker, setSpeaker] = useState(false);
-    const [isGray, setGray] = useState(
-        localStorage.getItem("grayMode") ? localStorage.getItem("grayMode") : false
-    );
+  const { pathname } = useLocation();
+  const { children } = props;
+  const [text, setText] = useState("");
+  const [speaker, setSpeaker] = useState(false);
+  const [isGray, setGray] = useState(
+    localStorage.getItem("grayMode") ? localStorage.getItem("grayMode") : false
+  );
 
-    const registerHeader = pathname.split("/")[1]
+  const registerHeader = pathname.split("/")[1];
 
-    const changeSpeakSwitcher = (value) => {
-        setSpeaker(value);
+  const changeSpeakSwitcher = (value) => {
+    setSpeaker(value);
+  };
+
+  const grayScale = () => {
+    setGray(!isGray);
+    localStorage.setItem("grayMode", !isGray);
+  };
+
+  const htmlTag = document.getElementsByTagName("html");
+
+  JSON.parse(isGray)
+    ? htmlTag[0].classList.add("gray-scale")
+    : htmlTag[0].classList.remove("gray-scale");
+
+  useEffect(() => {
+    document.onmouseup = () => {
+      if (speaker && text !== window.getSelection().toString()) {
+        window.responsiveVoice.speak(
+          window.getSelection().toString(),
+          "Russian Female"
+        );
+        setText(window.getSelection().toString());
+      }
     };
+    //eslint-disable-next-line
+  }, [speaker]);
 
-    const grayScale = () => {
-        setGray(!isGray);
-        localStorage.setItem("grayMode", !isGray);
-    };
-
-    const htmlTag = document.getElementsByTagName("html");
-
-    JSON.parse(isGray)
-        ? htmlTag[0].classList.add("gray-scale")
-        : htmlTag[0].classList.remove("gray-scale");
-
-    useEffect(() => {
-        document.onmouseup = () => {
-            if (speaker && text !== window.getSelection().toString()) {
-                window.responsiveVoice.speak(
-                    window.getSelection().toString(),
-                    "Russian Female"
-                );
-                setText(window.getSelection().toString());
-            }
-        };
-        //eslint-disable-next-line
-    }, [speaker]);
-
-    return (
-        <GrayContext.Provider value={{ isGray, grayScale }}>
-            <NetworkError />
-            <div className={registerHeader.includes("registration") ? "registerBackground" : "page-wrapper1"}>
-                {registerHeader.includes("portal") ? (
-                    <HeaderPortal />
-                ) : (
-                    registerHeader.includes("registration") ? <RegisterHeader /> : null
-                )}
-                <div className="page-content">{children}</div>
-                {registerHeader.includes("portal") ? null : registerHeader.includes("registration")? <RegisterFooter /> : <Footer />}
-            </div>
-        </GrayContext.Provider>
-    );
+  return (
+    <GrayContext.Provider value={{ isGray, grayScale }}>
+      <NetworkError />
+      <div
+        className={
+          registerHeader.includes("registration")
+            ? "registerBackground"
+            : registerHeader.includes("portal")
+            ? ""
+            : "page-wrapper1"
+        }
+      >
+        {registerHeader.includes("portal") ? (
+          <HeaderPortal />
+        ) : registerHeader.includes("registration") ? (
+          <RegisterHeader />
+        ) : null}
+        <div className="page-content">{children}</div>
+        {registerHeader.includes("portal") ? null : registerHeader.includes(
+            "registration"
+          ) ? (
+          <RegisterFooter />
+        ) : (
+          <Footer />
+        )}
+      </div>
+    </GrayContext.Provider>
+  );
 };
 
 export default Layout;
