6e6df06859a61d2ddcc052adadfdfcb63bdbf04c
Shahriddinov <shahriddinovmurod195@gmail.com>
2023-03-17 10:57:28 +0500

    AboutPages

--- src/component/Layout/Layout.jsx
+++ src/component/Layout/Layout.jsx
@@ -10,92 +10,58 @@ import RegisterFooter from "../../pages/Registration/pages/Layout/Footer/Registe
 export const GrayContext = createContext();
 
 const Layout = (props) => {
-  const { pathname } = useLocation();
-  const { children } = props;
-  const [text, setText] = useState("");
-  const [speaker, setSpeaker] = useState(false);
-  const [isGray, setGray] = useState(
-    localStorage.getItem("grayMode") ? localStorage.getItem("grayMode") : false
-  );
+    const { pathname } = useLocation();
+    const { children } = props;
+    const [text, setText] = useState("");
+    const [speaker, setSpeaker] = useState(false);
+    const [isGray, setGray] = useState(
+        localStorage.getItem("grayMode") ? localStorage.getItem("grayMode") : false
+    );
 
-  const registerHeader = pathname.split("/")[1];
+    const registerHeader = pathname.split("/")[1]
 
-  const changeSpeakSwitcher = (value) => {
-    setSpeaker(value);
-  };
-
-  const grayScale = () => {
-    setGray(!isGray);
-    localStorage.setItem("grayMode", !isGray);
-  };
-
-  const htmlTag = document.getElementsByTagName("html");
-
-  JSON.parse(isGray)
-    ? htmlTag[0].classList.add("gray-scale")
-    : htmlTag[0].classList.remove("gray-scale");
-
-  useEffect(() => {
-    document.onmouseup = () => {
-      if (speaker && text !== window.getSelection().toString()) {
-        window.responsiveVoice.speak(
-          window.getSelection().toString(),
-          "Russian Female"
-        );
-        setText(window.getSelection().toString());
-      }
+    const changeSpeakSwitcher = (value) => {
+        setSpeaker(value);
     };
-    //eslint-disable-next-line
-  }, [speaker]);
 
-  return (
-    <GrayContext.Provider value={{ isGray, grayScale }}>
-      <NetworkError />
-      <div
-        className={
-          pathname === "/"
-            ? "page-wrapper1"
-            : pathname === "/about"
-            ? "page-about"
-            : pathname === "/about/council-trustees"
-            ? "page-about"
-            : pathname === "/registration/register" ||
-              pathname === "/registration/signup" ||
-              pathname === "/registration/signin" ||
-              pathname === "/registration/recovery-password" ||
-              pathname === "/registration/change-password"
-            ? "registerBackground"
-            : "page-wrapper2"
-        }
-      >
-        {pathname === "/portal" ? (
-          <HeaderPortal />
-        ) : pathname === "/registration" ? (
-          <RegisterHeader />
-        ) : pathname === "/registration/register" ||
-          pathname === "/registration/signup" ||
-          pathname === "/registration/signin" ||
-          pathname === "/registration/recovery-password" ||
-          pathname === "/registration/change-password" ? (
-          <RegisterHeader />
-        ) : (
-          <Header speaker={speaker} changeSpeakSwitcher={changeSpeakSwitcher} />
-        )}
-        <div className="page-content">{children}</div>
-        {pathname === "/portal" ? (
-          ""
-        ) : pathname === "/registration/register" ||
-          pathname === "/registration/signup" ||
-          pathname === "/registration/signin" ||
-          pathname === "/registration/recovery-password" ||
-          pathname === "/registration/change-password" ? (
-          <RegisterFooter />
-        ) : (
-          <Footer />
-        )}
-      </div>
-    </GrayContext.Provider>
-  );
+    const grayScale = () => {
+        setGray(!isGray);
+        localStorage.setItem("grayMode", !isGray);
+    };
+
+    const htmlTag = document.getElementsByTagName("html");
+
+    JSON.parse(isGray)
+        ? htmlTag[0].classList.add("gray-scale")
+        : htmlTag[0].classList.remove("gray-scale");
+
+    useEffect(() => {
+        document.onmouseup = () => {
+            if (speaker && text !== window.getSelection().toString()) {
+                window.responsiveVoice.speak(
+                    window.getSelection().toString(),
+                    "Russian Female"
+                );
+                setText(window.getSelection().toString());
+            }
+        };
+        //eslint-disable-next-line
+    }, [speaker]);
+
+    return (
+        <GrayContext.Provider value={{ isGray, grayScale }}>
+            <NetworkError />
+            <div className={registerHeader.includes("registration") ? "registerBackground" : "page-wrapper1"}>
+                {registerHeader.includes("portal") ? (
+                    <HeaderPortal />
+                ) : (
+                    registerHeader.includes("registration") ? <RegisterHeader /> : null
+                )}
+                <div className="page-content">{children}</div>
+                {registerHeader.includes("portal") ? null : registerHeader.includes("registration")? <RegisterFooter /> : <Footer />}
+            </div>
+        </GrayContext.Provider>
+    );
 };
 
 export default Layout;
