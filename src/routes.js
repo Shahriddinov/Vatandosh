6929d80c11cd761440a8d98aed2ff6bb8f20676e
Jasur <76224872+JasurNurgiljonov@users.noreply.github.com>
2023-03-20 13:49:43 +0500

    Loyihalar, responsive

--- src/routes.js
+++ src/routes.js
@@ -16,6 +16,7 @@ const PublicAssociations = lazy(() => import("./pages/compatriots"));
 const StatesFriendshipSociety = lazy(() => import("./pages/compatriots/statesFriendshipSociety/StatesFriendshipSociety"));
 const Portal = lazy(() => import("./pages/Portal/HomePage/HomePage"));
 const PublicAssociationEvents = lazy(() => import("./pages/PublicAssociationEvents/PublicAssociationEvents"));
+const Projects = lazy(() => import("./pages/Projects"));
 const NotFound = lazy(() => import("./pages/404"));
 const routes = [
   { path: "", element: Home, },
@@ -31,6 +32,7 @@ const routes = [
   { path: "/registration/change-password", element: ChangePassword },
   { path: "/category-shows", element: CategoryShows },
   { path: "/compatriots/public-association-events", element: PublicAssociationEvents },
+  { path: "/projects", element: Projects },
 ];
 
 const RoutesContainer = () => (
