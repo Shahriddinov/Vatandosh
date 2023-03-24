96dd11f23b4a26a97fa0f3e840e3a0f41278e080
eexiy <alojonsuyarov@gmail.com>
2023-03-24 15:53:36 +0500

    add page information services

--- src/component/Paginator/Paginator.jsx
+++ src/component/Paginator/Paginator.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
-import './Pagination.scss'
+import './Paginator.scss'
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 
 
 export const Paginator = () => {
-    const [page,setPage] = useState(1)
+    const [page, setPage] = useState(1)
 
     const handleChange = (_, value) => {
         setPage(value)
