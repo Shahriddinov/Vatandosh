06bc31c709cde88e2715f4cd3bd2457b5f5b3b5e
Jo Fay <jofay91@gmail.com>
2023-03-29 11:10:48 +0500

    Kirill lang added

--- src/services/i18n/resorce.js
+++ src/services/i18n/resorce.js
@@ -1,7 +1,7 @@
 import uz from "../../Ing/uz.json";
 import ru from "../../Ing/ru.json";
 import en from "../../Ing/en.json";
-
+import ўз from "../../Ing/ўз.json";
 
 export const resorce = {
     uz: {
@@ -12,5 +12,8 @@ export const resorce = {
     },
     en: {
         translation: en,
+    },
+    ўз: {
+        translation: ўз,
     }
 }
