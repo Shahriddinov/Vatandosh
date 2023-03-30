43a56ad052bdcfb46ff781471fd5c5ba9c0e32bf
eexiy <alojonsuyarov@gmail.com>
2023-03-30 13:00:11 +0500

    fix slider at InformationServices

--- src/pages/InformationServices/InformationServicesComponent/InformationServicesComponent.jsx
+++ src/pages/InformationServices/InformationServicesComponent/InformationServicesComponent.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 import "./InformationServicesComponent.scss";
+import { useTranslation } from 'react-i18next';
 
 const InformationServicesComponent = ({ pathPages }) => {
   const { pageName } = useParams();
 
+  const { t } = useTranslation()
+
   return (
     <div className="informationServicesComponent">
-      <h2 className="informationServicesComponent-title">Axborot xizmati</h2>
+      <h2 className="informationServicesComponent-title">{t("footerService")}</h2>
       <ul className="informationServicesComponent-list">
         {pathPages
           .filter((link) => link.link !== pageName)
