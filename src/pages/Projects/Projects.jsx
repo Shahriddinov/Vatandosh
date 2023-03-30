dbf21e32464e00602fe677af38d687c2243ee7a4
Jasur <76224872+JasurNurgiljonov@users.noreply.github.com>
2023-03-30 09:55:12 +0500

    Ishtirok etish modal

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -5,6 +5,7 @@ import "slick-carousel/slick/slick-theme.css";
 import './projects.scss'
 import SliderData from './data'
 import Gallery from './gallery'
+import ParticipateModal from './Participate/Participate';
 
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -31,6 +32,18 @@ const Projects = () => {
 
     const sliderRef = useRef(null);
     const swiperRef = useRef(null);
+
+    const [modalOn, setModalOn] = useState(false);
+    
+    const toggleModal = () => {
+        setModalOn(!modalOn)
+        if (window.innerWidth <= 1000) {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        }
+    }
 
     const [numVisibleDivs, setNumVisibleDivs] = useState(0);
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -158,105 +171,109 @@ const Projects = () => {
     }
 
     return (
-        <div className="projects">
-            <div className="projects__top">
-                <WhriteHeader />
-                <CouncilHero {...heroData} />
-            </div>
-            <div className="projects__body container">
-                <div className="projects__all">
-                    <div className="top">
-                        <div className="title">
-                            <h1>Biznig barcha loyihalar</h1>
-                            <p>Chet elda yashayotgan yurtdoshlarimizni hayoti bilan ham nafas bolishingiz mumkin!</p>
-                        </div>
-                        <div className="carousel__buttons">
-                            <button className="carousel__button" onClick={() => sliderRef.current.slickPrev()}>
-                            <FiChevronLeft size={20} />
+        <>
+            {!modalOn && window.innerWidth <= 1000 &&
+            <div className="projects">
+                <div className="projects__top">
+                    <WhriteHeader />
+                    <CouncilHero {...heroData} />
+                </div>
+                <div className="projects__body container">
+                    <div className="projects__all">
+                        <div className="top">
+                            <div className="title">
+                                <h1>Biznig barcha loyihalar</h1>
+                                <p>Chet elda yashayotgan yurtdoshlarimizni hayoti bilan ham nafas bolishingiz mumkin!</p>
+                            </div>
+                            <div className="carousel__buttons">
+                                <button className="carousel__button" onClick={() => sliderRef.current.slickPrev()}>
+                                <FiChevronLeft size={20} />
+                                    </button>
+                                <button className="carousel__button" onClick={() => sliderRef.current.slickNext()}>
+                                    <FiChevronRight size={20} />
                                 </button>
-                            <button className="carousel__button" onClick={() => sliderRef.current.slickNext()}>
-                                <FiChevronRight size={20} />
-                            </button>
+                            </div>
                         </div>
-                    </div>
-                    <Slider {...settings} ref={sliderRef}>
-                        { slicedData.map((element) => {
-                            return (
-                                <div className="slider__element" key={element.id}>
-                                    <div className="hashtag">
-                                        <div className="hashtag__images">
-                                            <img src={element.image1} alt="" />
-                                            <img src={element.image2} alt="" />
-                                            <img src={element.image3} alt="" />
-                                            <img src={element.image1} alt="" />
-                                        </div>
-                                        <div className="hashtag__info">
-                                            <h4>"{element.title}" rukni</h4>
-                                            <p>{element.dataCount} ta malumot mavjud</p>
+                        <Slider {...settings} ref={sliderRef}>
+                            { slicedData.map((element) => {
+                                return (
+                                    <div className="slider__element" key={element.id}>
+                                        <div className="hashtag">
+                                            <div className="hashtag__images">
+                                                <img src={element.image1} alt="" />
+                                                <img src={element.image2} alt="" />
+                                                <img src={element.image3} alt="" />
+                                                <img src={element.image1} alt="" />
+                                            </div>
+                                            <div className="hashtag__info">
+                                                <h4>"{element.title}" rukni</h4>
+                                                <p>{element.dataCount} ta malumot mavjud</p>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            )
-                        })
+                                )
+                            })
+                            }
+                        </Slider>
+                        { numVisibleDivs === SliderData.length ?
+                            <button className="show__more" onClick={handleShowLess}>
+                                <img src={ArrowDown} alt="" className='rotate180'/>
+                                Kamroq ko'rish
+                            </button>
+                            :
+                            <button className="show__more" onClick={handleShowMore}>
+                                <img src={ArrowDown} alt="" />
+                                Ko'proq ko'rish
+                            </button>
                         }
-                    </Slider>
-                    { numVisibleDivs === SliderData.length ?
-                        <button className="show__more" onClick={handleShowLess}>
-                            <img src={ArrowDown} alt="" className='rotate180'/>
-                            Kamroq ko'rish
-                        </button>
-                        :
-                        <button className="show__more" onClick={handleShowMore}>
-                            <img src={ArrowDown} alt="" />
-                            Ko'proq ko'rish
-                        </button>
-                    }
-                    <div className="participate">
-                        <div className="participate__left">
-                            <img src={Pattern1} alt="" className='pattern__left'/>
-                            <img src={Pattern2} alt="" className='pattern__center'/>
-                            <img src={Pattern3} alt="" className='pattern__right'/>
-                            <h3>Loyihada ishtirok eting</h3>
-                            <p>Bizning loyihalarda ishtirok etish istagingiz bo‘lsa ariza to‘ldiriring va loyiha ishtirokchisiga aylaning!</p>
-                            <button>Ishtirok etish</button>
+                        <div className="participate">
+                            <div className="participate__left">
+                                <img src={Pattern1} alt="" className='pattern__left'/>
+                                <img src={Pattern2} alt="" className='pattern__center'/>
+                                <img src={Pattern3} alt="" className='pattern__right'/>
+                                <h3>Loyihada ishtirok eting</h3>
+                                <p>Bizning loyihalarda ishtirok etish istagingiz bo‘lsa ariza to‘ldiriring va loyiha ishtirokchisiga aylaning!</p>
+                                <button onClick={toggleModal}>Ishtirok etish</button>
+                            </div>
+                            <img src={Participate} alt="" className='participate__img'/>
                         </div>
-                        <img src={Participate} alt="" className='participate__img'/>
                     </div>
                 </div>
-            </div>
-            <div className="projects__caruosel">
-                <div className="container carousel__title">
-                    <h1>Fotogalereya</h1>
+                <div className="projects__caruosel">
+                    <div className="container carousel__title">
+                        <h1>Fotogalereya</h1>
+                    </div>
+                    <Swiper
+                        ref={swiperRef}
+                        modules={[Navigation, Pagination, Scrollbar, A11y]}
+                        spaceBetween={50}
+                        slidesPerView={4}
+                        centeredSlides={true}
+                        initialSlide={1}
+                        onSlideChange={swiper => setCurrentIndex(swiper.activeIndex)}
+                        loop={true}
+                        {...swiperParams}
+                    >
+                        {Gallery.map((obj, index) => {
+                            return (
+                                <SwiperSlide key={index}>
+                                    <img src={obj.image} alt="" className='gallery__img'/>
+                                </SwiperSlide>
+                            )
+                        })}
+                    </Swiper>
+                    <div className="carousel__pagination carousel__buttons">
+                        <button className='carousel__button' onClick={prevSlide}><FiChevronLeft/></button>
+                        <button className='carousel__button'>{currentIndex}</button>
+                        <button className='carousel__button'>{currentIndex + 1}</button>
+                        <button className='carousel__button'>...</button>
+                        <button className='carousel__button'>{Gallery.length}</button>
+                        <button className='carousel__button' onClick={nextSlide}><FiChevronRight/></button>
+                    </div>
                 </div>
-                <Swiper
-                    ref={swiperRef}
-                    modules={[Navigation, Pagination, Scrollbar, A11y]}
-                    spaceBetween={50}
-                    slidesPerView={4}
-                    centeredSlides={true}
-                    initialSlide={1}
-                    onSlideChange={swiper => setCurrentIndex(swiper.activeIndex)}
-                    loop={true}
-                    {...swiperParams}
-                >
-                    {Gallery.map((obj, index) => {
-                        return (
-                            <SwiperSlide key={index}>
-                                <img src={obj.image} alt="" className='gallery__img'/>
-                            </SwiperSlide>
-                        )
-                    })}
-                </Swiper>
-                <div className="carousel__pagination carousel__buttons">
-                    <button className='carousel__button' onClick={prevSlide}><FiChevronLeft/></button>
-                    <button className='carousel__button'>{currentIndex}</button>
-                    <button className='carousel__button'>{currentIndex + 1}</button>
-                    <button className='carousel__button'>...</button>
-                    <button className='carousel__button'>{Gallery.length}</button>
-                    <button className='carousel__button' onClick={nextSlide}><FiChevronRight/></button>
-                </div>
-            </div>
-        </div>
+            </div>}
+            {modalOn && <><WhriteHeader /><ParticipateModal toggleModal={toggleModal}/></>}
+        </>
     )
 }
 
