0819123f6f4077a94a515997e700886831a6f8d7
G'ayrat Nurullayev <gayratnurullayev21@gmail.com>
2023-03-31 23:26:49 +0500

    change file

--- src/pages/boardTrustees/BoardTrustees.jsx
+++ src/pages/boardTrustees/BoardTrustees.jsx
@@ -1,37 +1,39 @@
-import React, { useEffect } from 'react'
-import "./boardTrustees.scss"
+import "./boardTrustees.scss";
 
-
-import CouncilHero from './components/council-hero/CouncilHero'
-import CouncilComposition from './components/council-composition/CouncilComposition'
-import ContactUs from '../../component/ContactUs/ContactUs'
-import { useDispatch } from 'react-redux'
-import { getAllTrustees } from '../../reduxToolkit/trusteesSlice/trusteesAsyncThunk'
-import Header from '../../component/Layout/Header/Header'
+import CouncilHero from "./components/council-hero/CouncilHero";
+import CouncilComposition from "./components/council-composition/CouncilComposition";
 import WhriteHeader from "../../component/Layout/WhriteHeader/WhriteHeader";
+import { Spinner } from "../../component";
+import { usePaginationFetching } from "./hooks/usePaginationFetfhing";
 
 const BoardTrustees = () => {
+  const { trusts, searchCount, handleFetching, trustsLoading } =
+    usePaginationFetching();
 
-  const dispatch = useDispatch()
   const heroData = {
-    title:"Vasiylik kengashi",
-    description:"Xorijda istiqomat qilayotgan vatandoshlarni tarixiy Vatani atrofida yanada jipslashtirish, ularning qalbi va ongida yurt bilan faxrlanish tuyg‘usini yuksaltirish, milliy o‘zlikni saqlab qolish, vatandoshlar va ular tomonidan tuzilgan jamoat birlashmalarini qo‘llab-quvvatlash, turli sohalarda faoliyat yuritayotgan vatandoshlarimizning salohiyatini mamlakatimiz taraqqiyotiga samarali yo‘naltirish Fondning asosiy maqsadlaridan biri hisoblanadi.",
+    title: "Vasiylik kengashi",
+    description:
+      "Xorijda istiqomat qilayotgan vatandoshlarni tarixiy Vatani atrofida yanada jipslashtirish, ularning qalbi va ongida yurt bilan faxrlanish tuyg‘usini yuksaltirish, milliy o‘zlikni saqlab qolish, vatandoshlar va ular tomonidan tuzilgan jamoat birlashmalarini qo‘llab-quvvatlash, turli sohalarda faoliyat yuritayotgan vatandoshlarimizning salohiyatini mamlakatimiz taraqqiyotiga samarali yo‘naltirish Fondning asosiy maqsadlaridan biri hisoblanadi.",
     pagePath: "Vasiylik kengashi",
+  };
+
+  if (trustsLoading) {
+    return <Spinner position="full" />;
   }
 
-  useEffect(() => {
-    dispatch(getAllTrustees())
-  },[])
-  
   return (
     <div className="council-trustees">
       <div className="page-about">
-          <WhriteHeader/>
-          <CouncilHero  {...heroData}/>
+        <WhriteHeader />
+        <CouncilHero {...heroData} />
       </div>
-      <CouncilComposition/>
+      <CouncilComposition
+        trusts={trusts}
+        searchCount={searchCount}
+        handleFetching={handleFetching}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default BoardTrustees
+export default BoardTrustees;
