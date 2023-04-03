ce740a4303a6b51e0de9590319aa852db7560f36
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2023-04-03 14:31:26 +0500

    fixed bug

--- src/pages/Portal/pages/AboutCouncil/AboutCouncil.jsx
+++ src/pages/Portal/pages/AboutCouncil/AboutCouncil.jsx
@@ -1,12 +1,8 @@
-import Navbar from "../../components/Navbar/Navbar";
-import Nav from "../../components/Nav/Nav";
 import About from "./components/About/About";
 
 function AboutCouncil() {
   return (
     <div>
-      <Navbar />
-      <Nav />
       <About />
     </div>
   );
--- src/pages/Portal/pages/ExpertEmploye/ExpertEmploye.jsx
+++ src/pages/Portal/pages/ExpertEmploye/ExpertEmploye.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import Navbar from "../../components/Navbar/Navbar";
-import Nav from "../../components/Nav/Nav";
 import Employe from "./components/Employe/Employe";
 
 function ExpertEmploye() {
   return (
     <div>
-      <Navbar />
-      <Nav />
       <Employe />
     </div>
   );
