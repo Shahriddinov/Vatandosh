d12b98d5aee3e56828c3ca6403c920a5fa5c9a96
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2023-04-10 10:54:00 +0500

    language volunter

--- src/pages/Portal/volunter/VolunterLayout.jsx
+++ src/pages/Portal/volunter/VolunterLayout.jsx
@@ -2,27 +2,29 @@ import React from "react";
 import ExpertHeader from "../expert/components/ExpertHeader/ExpertHeader";
 import ExpertFooter from "../expert/components/ExpertFooter/ExpertFooter";
 import { Outlet, useLocation } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 export default function VolunterLayout() {
   const location = useLocation();
   const editClass = location.pathname.split("/");
+  const { t } = useTranslation();
 
   const navData = [
     { id: 1, url: "/portal-category/volunteer", label: "Asosiy" },
     {
       id: 2,
       url: "/portal-category/volunteer/volunter-employe",
-      label: "Volontyorlar",
+      label: `${t("volunter.nav1")}`,
     },
     {
       id: 3,
       url: "/portal-category/volunteer/council-about",
-      label: "Volontyorlarni faoliyati",
+      label: `${t("volunter.nav2")}`,
     },
     {
       id: 4,
       url: "/portal-category/volunteer/contact",
-      label: "Bog‘lanish",
+      label: `${t("volunter.nav3")}`,
     },
   ];
 
