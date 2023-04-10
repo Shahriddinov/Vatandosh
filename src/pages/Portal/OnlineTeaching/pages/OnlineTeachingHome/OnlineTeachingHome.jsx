555b268db523e0ad41586b09155bf695e3fbf604
Shahriddinov <shahriddinovmurod195@gmail.com>
2023-04-10 21:55:46 +0500

    firs

---
+++ src/pages/Portal/OnlineTeaching/pages/OnlineTeachingHome/OnlineTeachingHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import OnlineTeachingBody from "./OnlineTeachingBody/OnlineTeachingBody";
+
+function OnlineTeachingHome(props) {
+    return (
+        <div>
+            <OnlineTeachingBody/>
+        </div>
+    );
+}
+
+export default OnlineTeachingHome;
