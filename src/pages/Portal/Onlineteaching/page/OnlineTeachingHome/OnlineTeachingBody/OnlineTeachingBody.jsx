d9fdd05bb1566f403e2b93c6d0cbc33253023011
Shahriddinov <shahriddinovmurod195@gmail.com>
2023-04-10 15:47:56 +0500

    change

--- src/pages/Portal/Onlineteaching/page/OnlineTeachingHome/OnlineTeachingBody/OnlineTeachingBody.jsx
+++ src/pages/Portal/Onlineteaching/page/OnlineTeachingHome/OnlineTeachingBody/OnlineTeachingBody.jsx
@@ -4,8 +4,37 @@ import Logos from "../../../../../../assets/images/OnlineTeaching/image.svg"
 import Logos2 from "../../../../../../assets/images/OnlineTeaching/image2.svg"
 import Teacher from "../../../../../../assets/images/OnlineTeaching/Teacher.svg"
 import Grades from "../../../../../../assets/images/OnlineTeaching/Grades.svg"
+import Online from "../../../../../../assets/images/OnlineTeaching/Online.svg"
+import Seminar from "../../../../../../assets/images/OnlineTeaching/Seminar.svg"
 
 function OnlineTeachingBody(props) {
+    const Levels = [
+        {
+            title: "Beginner",
+            text: "В этом курсе вы примете участие в серии задач, призванных повысить ваше собственное счастье и выработать более ",
+            send: "Начать урок",
+            photos: Teacher
+        },
+        {
+            title: "Pre-intermediate",
+            text: "В этом курсе вы примете участие в серии задач, призванных повысить ваше собственное счастье и выработать более ",
+            send: "Начать урок",
+            photos: Grades
+        },
+        {
+            title: "Intermediate",
+            text: "В этом курсе вы примете участие в серии задач, призванных повысить ваше собственное счастье и выработать более ",
+            send: "Начать урок",
+            photos: Online
+        },
+        {
+            title: "Advanced",
+            text: "В этом курсе вы примете участие в серии задач, призванных повысить ваше собственное счастье и выработать более ",
+            send: "Начать урок",
+            photos: Seminar
+        }
+
+    ]
     return (
         <div className="onlineTeachingBody container">
             <div className="onlineTeachingBody_logotip">
@@ -29,30 +58,18 @@ function OnlineTeachingBody(props) {
             </div>
 
             <div className="onlineTeachingBody_level">
-                <div className="onlineTeachingBody_level_start">
-                    <div className="onlineTeachingBody_level_start_cardInfo">
-                        <div className="onlineTeachingBody_level_start_cardInfo_titles">Beginner</div>
-                        <div className="onlineTeachingBody_level_start_cardInfo_describtions">В этом курсе вы примете
-                            участие в серии задач, призванных повысить ваше собственное счастье и выработать более
+                {Levels.map((item, index) => (
+                    <div key={index} className="onlineTeachingBody_level_start" >
+                        <div className="onlineTeachingBody_level_start_cardInfo" >
+                            <div className="onlineTeachingBody_level_start_cardInfo_titles">{item.title}</div>
+                            <div className="onlineTeachingBody_level_start_cardInfo_describtions">{item.text}</div>
+                            <div className="onlineTeachingBody_level_start_cardInfo_sends">{item.send}</div>
                         </div>
-                        <div className="onlineTeachingBody_level_start_cardInfo_sends">Начать урок</div>
-                    </div>
-                    <div className="onlineTeachingBody_level_start_cardInfo">
-                        <img className="onlineTeachingBody_level_start_cardInfo_teacher" src={Teacher} alt=""/>
-                    </div>
-                </div>
-                <div className="onlineTeachingBody_level_start">
-                    <div className="onlineTeachingBody_level_start_cardInfo">
-                        <div className="onlineTeachingBody_level_start_cardInfo_titles">Pre-intermediate</div>
-                        <div className="onlineTeachingBody_level_start_cardInfo_describtions">В этом курсе вы примете
-                            участие в серии задач, призванных повысить ваше собственное счастье и выработать более
+                        <div className="onlineTeachingBody_level_start_cardInfo">
+                            <img className="onlineTeachingBody_level_start_cardInfo_teacher" src={item.photos} alt=""/>
                         </div>
-                        <div className="onlineTeachingBody_level_start_cardInfo_sends">Начать урок</div>
                     </div>
-                    <div className="onlineTeachingBody_level_start_cardInfo">
-                        <img className="onlineTeachingBody_level_start_cardInfo_teacher" src={Grades} alt=""/>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
     );
