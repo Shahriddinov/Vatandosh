d9a978ad26abe6c7472fbd52d429ac243a17c130
KhushnudEshtemirov <khushnudeshtemirov@gmail.com>
2023-04-11 15:46:16 +0500

    Finished Online Teaching Hero

--- src/pages/Portal/OnlineTeaching/pages/OnlineTeachingHome/OnlineTeachingHome.jsx
+++ src/pages/Portal/OnlineTeaching/pages/OnlineTeachingHome/OnlineTeachingHome.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import OnlineTeachingBody from "./OnlineTeachingBody/OnlineTeachingBody";
+import React from "react";
+import OnlineTeachingBody from "../../components/OnlineTeachingBody/OnlineTeachingBody";
 import OnlineTeachingHero from "../../components/OnlineTeachingHero/OnlineTeachingHero";
 
 function OnlineTeachingHome(props) {
-    return (
-        <div>
-            <OnlineTeachingHero/>
-            <OnlineTeachingBody/>
-        </div>
-    );
+  return (
+    <div>
+      <OnlineTeachingHero />
+      <OnlineTeachingBody />
+    </div>
+  );
 }
 export default OnlineTeachingHome;
