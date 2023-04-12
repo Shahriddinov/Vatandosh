f4eb5d14cb6c8cf135c8bb7fda285d2b6dd345a3
Shahriddinov <shahriddinovmurod195@gmail.com>
2023-04-12 10:38:43 +0500

    changes

--- src/pages/Portal/OnlineTeaching/OnlineTeachingLayout.jsx
+++ src/pages/Portal/OnlineTeaching/OnlineTeachingLayout.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 import OnlineTeachingHeader from "./components/OnlineTeachingHeader/OnlineTeachingHeader";
-import OnlineTeachingFooter from "./components/OnlineTeachingFooter/OnlineTeachingFooter";
+// import OnlineTeachingFooter from "./components/OnlineTeachingFooter/OnlineTeachingFooter";
 
 const OnlineTeachingLayout = () => {
   return (
     <div>
       <OnlineTeachingHeader />
       <Outlet />
-      <OnlineTeachingFooter />
+      {/*<OnlineTeachingFooter />*/}
     </div>
   );
 };
