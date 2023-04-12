f00994516e7498e4d158c041d0a49e561a825639
Shahriddinov <shahriddinovmurod195@gmail.com>
2023-04-12 11:18:47 +0500

    changes Footer

--- src/pages/Portal/OnlineTeaching/components/OnlineTeachingFooter/OnlineTeachingFooter.jsx
+++ src/pages/Portal/OnlineTeaching/components/OnlineTeachingFooter/OnlineTeachingFooter.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import "./onlineTeachingFooter.scss";
+import FooterLogo from "../../../../../assets/images/online-teaching/FooterLogo.svg"
 
 function OnlineTeachingFooter(props) {
     return (
-        <div>
-            OnlineTeachingFooter
+        <div className="OnlineTeachingFooter">
+            <div className="OnlineTeachingFooter_boxes container">
+                <div className="onlineTeachingFooter_boxes_betweens">
+                    Murod
+                </div>
+            </div>
         </div>
     );
 }
