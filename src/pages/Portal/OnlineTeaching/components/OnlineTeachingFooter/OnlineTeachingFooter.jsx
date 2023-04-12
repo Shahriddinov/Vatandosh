5b1cffaf17c913844cbb660db376aebac84e715f
Shahriddinov <shahriddinovmurod195@gmail.com>
2023-04-12 10:42:16 +0500

    changes

---
+++ src/pages/Portal/OnlineTeaching/components/OnlineTeachingFooter/OnlineTeachingFooter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function OnlineTeachingFooter(props) {
+    return (
+        <div>
+            OnlineTeachingFooter
+        </div>
+    );
+}
+
+export default OnlineTeachingFooter;
