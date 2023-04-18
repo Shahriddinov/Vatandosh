c6aa9bfc8317c69574927f05e8d4a8eff2fd4d94
Shahriddinov <shahriddinovmurod195@gmail.com>
2023-04-18 14:12:06 +0500

    teachingbady

--- src/pages/Portal/OnlineTeaching/pages/AboutTeaching/AboutTeaching.jsx
+++ src/pages/Portal/OnlineTeaching/pages/AboutTeaching/AboutTeaching.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import AboutTeachingHero from "../../components/AboutTeachingHero/AboutTeachingHero";
 import "./aboutTeaching.scss"
+import Done from "../../../../../assets/images/online-teaching/done.svg"
 function AboutTeaching(props) {
     return (
         <>
             <AboutTeachingHero/>
             <div className="aboutTeaching container">
-                Murod
+               <div className="aboutTeaching_done">Чему вы научитесь</div>
+                <div className="aboutTeaching_cards">
+                    <div className="aboutTeaching_cards_steps">
+                        <div className="aboutTeaching_cards_steps_borders">
+                            <img className="aboutTeaching_cards_steps_borders_imgs" src={Done} alt=""/>
+                        </div>
+                        <div className="aboutTeaching_cards_steps_discrib">
+                           <div className="aboutTeaching_cards_steps_discrib_title">100% онлайн</div>
+
+                        </div>
+                    </div>
+                    <div className="aboutTeaching_cards_steps">Mutrod</div>
+                </div>
             </div>
         </>
     );
