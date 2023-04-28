39f05601a9ac6e6c5055f413c5521c1935bdcfeb
Shahriddinov <shahriddinovmurod195@gmail.com>
2023-04-28 15:03:28 +0500

    404

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import znak from "../../assets/images/znak.png";
 import znakLeft from "../../assets/images/znak-left.png";
+import Erorr from "../../assets/images/404.svg"
 import "./style.scss";
 import Header from "../../component/Layout/Header/Header";
 import Footer from "../../component/Layout/Footer/Footer";
@@ -27,15 +28,15 @@ const NotFound = () => {
       <div className="container">
         <div className="not-found">
           <div className="img-box">
-            <img
-              className="back-image"
-              src={require("../../assets/images/404-png.png")}
-              alt="earth"
-            />
-            <img className="znak-left" src={znakLeft} alt="znak" />
-            <img className="znak" src={znak} alt="znak2" />
+            {/*<img*/}
+            {/*  className="back-image"*/}
+            {/*  src={require("../../assets/images/404-png.png")}*/}
+            {/*  alt="earth"*/}
+            {/*/>*/}
+            <img className="back-image" src={Erorr} alt="znak" />
+            {/*<img className="znak" src={znak} alt="znak2" />*/}
           </div>
-          <h2 className="title">Уппс Мы не можем найти эту страницу</h2>
+          <h2 className="title">Ync! Cтраница не найдена</h2>
           <Link to="/" className="go-home-btn">
             Домой
           </Link>
