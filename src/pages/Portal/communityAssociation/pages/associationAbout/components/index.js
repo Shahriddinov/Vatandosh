889ce4a56555e69e7b8661e1f722af84e9fa550e
gnurullayev <nurullayevgayrat73@gmail.com>
2023-05-03 16:27:58 +0500

    add portal comunity page api finished

---
+++ src/pages/Portal/communityAssociation/pages/associationAbout/components/index.js
@@ -0,0 +1,4 @@
+import CommunityPageTop from "./communityPageTop/CommunityPageTop";
+import CommunityCouncilStatics from "./comunityCouncilStatics/CommunityCouncilStatics";
+
+export { CommunityCouncilStatics, CommunityPageTop };
---
+++ src/pages/Portal/communityAssociation/pages/associationHome/components/homeCouncil/HomeCouncil.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { useInView } from "react-intersection-observer";
+import { Link } from "react-router-dom";
+import { CommunityCouncilStatics } from "../../../associationAbout/components";
+import { LazySpinner } from "../../../../../../../component";
+import DOMPurify from "dompurify";
+
+import "./communityHomeCouncil.scss";
+
+const CommunityHomeCouncil = ({ councilData }) => {
+  const { ref, inView } = useInView({
+    threshold: 0,
+    triggerOnce: true,
+  });
+  const { t } = useTranslation();
+  return (
+    <div className="community-home-council">
+      <div className="container" ref={ref}>
+        {inView ? (
+          <>
+            <div className="community-home-council__left">
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(councilData.body),
+                }}
+              />
+              <div className="community-home-council__left_btn">
+                <Link to={councilData.pathUrl}>{t("expert.detail")}</Link>
+              </div>
+            </div>
+            <CommunityCouncilStatics />
+          </>
+        ) : (
+          <LazySpinner height="350px" />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CommunityHomeCouncil;
