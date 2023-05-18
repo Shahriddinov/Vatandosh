7184aff6325028daa258d8eed7bb1ca6b99bfb7a
Jakhongir <jakhongiryusupov58@gmail.com>
2023-05-18 17:39:54 +0500

    about us and compatriots bags cleared

--- src/pages/About/component/Direction/BasicDirection.jsx
+++ src/pages/About/component/Direction/BasicDirection.jsx
@@ -1,56 +1,57 @@
-import React, {useEffect} from 'react';
-import "./style.scss"
+import React, { useEffect } from "react";
+import "./style.scss";
 import WhriteHeader from "../../../../component/Layout/WhriteHeader/WhriteHeader";
 import CouncilHero from "../../../boardTrustees/components/council-hero/CouncilHero";
-import {useTranslation} from "react-i18next";
-import {getDirection} from "../../../../reduxToolkit/Direction/Direction";
-import {useSelector, useDispatch} from "react-redux";
-import {baseServerUrl} from "../../../../services/api/utils";
+import { useTranslation } from "react-i18next";
+import { getDirection } from "../../../../reduxToolkit/Direction/Direction";
+import { useSelector, useDispatch } from "react-redux";
+import { baseServerUrl } from "../../../../services/api/utils";
 
 function BasicDirection(props) {
-    const {t} = useTranslation();
-    const dispatch = useDispatch();
-    const directionsData = useSelector((state) => state.directionSlice.directionData);
-    const lan = useSelector((state) => state.language.language);
-    const error = useSelector((state) => state.directionSlice.error);
-    const errorDirection = useSelector((state) => state.directionSlice.error);
-    const heroData = {
-        title: `Fond faoliyatining asosiy yo‘nalishlari`,
-        description: `Xorijda istiqomat qilayotgan vatandoshlarni tarixiy Vatani atrofida yanada jipslashtirish, ularning qalbi va ongida yurt bilan faxrlanish tuyg‘usini yuksaltirish, milliy o‘zlikni saqlab qolish, vatandoshlar va ular tomonidan tuzilgan jamoat birlashmalarini qo‘llab-quvvatlash, turli sohalarda faoliyat yuritayotgan vatandoshlarimizning salohiyatini mamlakatimiz taraqqiyotiga samarali yo‘naltirish Fondning asosiy maqsadlaridan biri hisoblanadi.`,
-        pagePath: `Yo'nalishlar`,
-    }
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const directionsData = useSelector(
+    (state) => state.directionSlice.directionData
+  );
+  const lan = useSelector((state) => state.language.language);
+  const error = useSelector((state) => state.directionSlice.error);
+  const errorDirection = useSelector((state) => state.directionSlice.error);
+  const heroData = {
+    title: `Fond faoliyatining asosiy yo‘nalishlari`,
+    pagePath: `Yo'nalishlar`,
+  };
 
-    useEffect(() => {
-        dispatch(getDirection());
-    }, []);
+  useEffect(() => {
+    dispatch(getDirection());
+  }, [dispatch]);
 
-    if (error || errorDirection) {
-        return <p>{error}</p>;
-    }
+  if (error || errorDirection) {
+    return <p>{error}</p>;
+  }
 
-    return (
-        <>
-            <div className="page-about">
-                <WhriteHeader/>
-                <CouncilHero  {...heroData}/>
+  return (
+    <>
+      <div className="page-about">
+        <WhriteHeader />
+        <CouncilHero {...heroData} />
+      </div>
+      <div className="about_direction_cards container">
+        {directionsData?.map((item, index) => (
+          <div key={item.id} className="about_direction_cards_boxs">
+            <img src={`${baseServerUrl}/${item.img}`} alt="partner" />
+            <div className="about_direction_cards_boxs_describtion">
+              {item[`name_${lan}`]}
             </div>
-
-            <div className="about_basics container">Asosiy yo'nalishlar</div>
-            <div className="about_direction_cards container">
-
-                {directionsData?.map((item, index) => (
-
-                    <div key={item.id} className="about_direction_cards_boxs">
-                        <img src={`${baseServerUrl}/${item.img}`} alt="partner"/>
-                        <div className="about_direction_cards_boxs_describtion">{item[`name_${lan}`]}</div>
-                        <img className="about_direction_cards_boxs_img" src={`${baseServerUrl}/${item.bgimg}`} alt=""/>
-                    </div>
-                ))}
-
-
-            </div>
-        </>
-    );
+            <img
+              className="about_direction_cards_boxs_img"
+              src={`${baseServerUrl}/${item.bgimg}`}
+              alt=""
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default BasicDirection;
--- src/pages/Portal/components/CustomProfil/CustomProfil.jsx
+++ src/pages/Portal/components/CustomProfil/CustomProfil.jsx
@@ -99,7 +99,7 @@ export default function CustomProfil() {
                       );
                     else
                       return (
-                        <div className="customprofil-list-otm-item">
+                        <div key={el.id} className="customprofil-list-otm-item">
                           <div className="customprofil-list-otm-desc">
                             <span>{t("expert.xorotm")}</span>
                             <p>{el?.institution}</p>
@@ -130,7 +130,7 @@ export default function CustomProfil() {
           <AccordionDetails>
             {employment.length
               ? employment.map((el) => (
-                  <div className="customprofil-list-workexp">
+                  <div key={el.id} className="customprofil-list-workexp">
                     <div className="customprofil-list-workexp-item">
                       <span>{t("expert.workspace")}</span>
                       <p>{el?.company}</p>
