b4fe67b1c899a9b43087affde59cedde378d58d7
gnurullayev <nurullayevgayrat73@gmail.com>
2023-05-29 15:07:58 +0500

    change

--- src/helpers/extraFunction.js
+++ src/helpers/extraFunction.js
@@ -11,3 +11,51 @@ export const htmlElement = (html) => {
 
   return { imgSrc, imgAlt, filteredText };
 };
+
+export const timer = ({ time, setDays, setHours, setMinutes }) => {
+  const targetDate = new Date(time);
+  let day = null;
+  let hours = null;
+  let minutes = null;
+
+  const updateCountdown = () => {
+    const currentTime = new Date().getTime();
+    const remainingTime = targetDate - currentTime;
+
+    if (remainingTime > 0) {
+      const remainingDays = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+      const remainingHours = Math.floor(
+        (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const remainingMinutes = Math.floor(
+        (remainingTime % (1000 * 60 * 60)) / (1000 * 60)
+      );
+
+      setDays(formatTime(remainingDays));
+      setHours(formatTime(remainingHours));
+      setMinutes(formatTime(remainingMinutes));
+    }
+  };
+
+  const formatTime = (time) => {
+    return time < 10 ? `0${time}` : `${time}`;
+  };
+
+  const fadeOutIn = () => {
+    const elements = document.getElementsByClassName("headertime-box");
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].classList.add("fade-out");
+    }
+
+    setTimeout(() => {
+      for (let i = 0; i < elements.length; i++) {
+        elements[i].classList.remove("fade-out");
+      }
+    }, 500);
+
+    setTimeout(fadeOutIn, 1000);
+  };
+
+  updateCountdown();
+  fadeOutIn();
+};
