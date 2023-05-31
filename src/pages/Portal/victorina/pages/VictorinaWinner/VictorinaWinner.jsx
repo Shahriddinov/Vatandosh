edc1964ffe203c2c81c2ece71027ba55a12aa81a
gnurullayev <nurullayevgayrat73@gmail.com>
2023-05-31 18:14:05 +0500

    change file

--- src/pages/Portal/victorina/pages/VictorinaWinner/VictorinaWinner.jsx
+++ src/pages/Portal/victorina/pages/VictorinaWinner/VictorinaWinner.jsx
@@ -1,6 +1,5 @@
 import { useTranslation } from "react-i18next";
 import "./VictorinaWinner.scss";
-import CouncilStatics from "../../../expert/pages/ExpertHome/components/Council/CouncilStatics";
 import ShareFriends from "../../../../../component/ShareFriends/ShareFriends";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,15 +9,32 @@ import { Link } from "react-router-dom";
 import { getQuizPage } from "../../../../../reduxToolkit/victorinapage/victorina-page";
 import { useEffect } from "react";
 import VictorinaStatics from "../VictorinaHome/components/VictorinaStatics/VictorinaStatics";
+import { Spinner } from "../../../../../component";
+import { getByIdQuizz } from "../../../../../reduxToolkit/victorinaQuiz/quizbyid/quizid";
 
 export default function VictorinaWinner() {
   const { t } = useTranslation();
-  const { id } = useParams();
-  const dispatch = useDispatch()
-  const quizData = useSelector((state) =>
-    state.quizSlice.quizData.participants?.find((evt) => evt?.id === Number(id))
-  );
+  const { id, victorinaById } = useParams();
+  const dispatch = useDispatch();
+  const quizData = useSelector((state) => state.quizByIdSlice.quizByIdData);
+  const quizDataLoading = useSelector((state) => state.quizByIdSlice.loading);
+  const quizDataError = useSelector((state) => state.quizSlice.error);
+
   const pageData = useSelector((state) => state.pageSlice.pageData);
+
+  const lan = useSelector((state) => state.language.language);
+
+  useEffect(() => {
+    dispatch(getByIdQuizz({ id: victorinaById }));
+    dispatch(getQuizPage());
+  }, [lan, dispatch, victorinaById]);
+
+  if (quizDataLoading) {
+    return <Spinner position="full" />;
+  } else if (quizDataError) {
+    return <p>{quizDataError}</p>;
+  }
+  const data = quizData?.participants?.find((evt) => evt?.id === Number(id));
 
   const url = [
     { title: t("expert.main"), url: "/portal-category/victorina" },
@@ -27,20 +43,17 @@ export default function VictorinaWinner() {
       url: "/portal-category/victorina/finished-projects",
     },
     {
-      title: quizData.quiz.title,
+      title: quizData?.title,
       url: "/portal-category/victorina/finished-projects/image-project",
     },
     { title: t("victorina.winner"), url: "" },
   ];
 
-  useEffect(() => {
-    dispatch(getQuizPage());
-  }, []);
   return (
     <main className="victorinawinner">
       <div className="container">
         <div className="experttitle-title">
-          <h1 className="experttitle-title-text">{quizData?.quiz?.title}</h1>
+          <h1 className="experttitle-title-text">{quizData?.title}</h1>
           <div className="experttitle-title-url">
             {url?.map((el, index) => (
               <Link key={el.id} to={el.url}>
@@ -54,18 +67,18 @@ export default function VictorinaWinner() {
           <div className="victorinawinner-main">
             <img
               className="victorinawinner-winner-img"
-              src={`${imageUrl}/${quizData?.quiz?.image}`}
+              src={`${imageUrl}/${quizData?.image}`}
               alt="error"
             />
             <div className="victorinawinner-main-profile">
-              <img src={`${imageUrl}/${quizData?.user?.avatar}`} alt="error" />
+              <img src={`${imageUrl}/${data?.user?.avatar}`} alt="error" />
               <div className="victorinawinner-main-profile-desc">
-                <p>{quizData.user.name}</p>
-                <span>{quizData.position}-o‘rin g‘olibi</span>
+                <p>{data.user.name}</p>
+                <span>{data.position}-o‘rin g‘olibi</span>
               </div>
             </div>
             <h3>{t("victorina.winnerprice")}</h3>
-            <p>{quizData.description}</p>
+            <p>{data?.description}</p>
             <ShareFriends />
           </div>
           <VictorinaStatics pageData={pageData} />
