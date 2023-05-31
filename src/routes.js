1be196c7af4aa218a8a42d936ade33c2a7bb0f8a
Jo Fay <jofay91@gmail.com>
2023-05-31 16:31:51 +0500

    moreDetail nav added

--- src/routes.js
+++ src/routes.js
@@ -404,6 +404,9 @@ const ChoicesPublicAssociations = lazy(() =>
   import("./pages/choices/publicAssociations/PublicAssociations")
 );
 const ChoicesQuizzes = lazy(() => import("./pages/choices/quizzes/quizzes"));
+const MoreDetail = lazy(() =>
+  import("./pages/choices/quizzes/moreDetail/MoreDetail")
+);
 
 const routes = [
   { path: "", element: Home },
@@ -440,6 +443,7 @@ const routes = [
   { path: "/registration/signup/api/verify/", element: EmailVerify },
   { path: "/registration/signup/api/reset/", element: ResetPassword },
   { path: "/choices/quiz", element: ChoicesQuizzes },
+  { path: "/choices/quiz/more-detail/:id/:status", element: MoreDetail },
   { path: "/choices/public-associations", element: ChoicesPublicAssociations },
 ];
 
