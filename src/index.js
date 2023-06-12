1ec0f432d55ae72a0f243ba26870d06d37929853
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2023-06-12 11:04:46 +0500

    commit

--- src/index.js
+++ src/index.js
@@ -15,9 +15,6 @@ import "./assets/style/Header.scss";
 import "animate.css/animate.min.css";
 import App from "./App";
 
-
-
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
