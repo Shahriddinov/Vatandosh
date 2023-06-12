a5df5207286c8e658ad239679d8be1df9720dc29
Shahriddinov <shahriddinovmurod195@gmail.com>
2023-06-12 10:13:31 +0500

    fisrt

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./reduxToolkit/store";
 import Routes from "./routes";
-import i18 from "./services/i18n/i18n";
 import GrayContextProvider from "./context/GrayContext";
-import { Spinner } from "./component";
-
 import "swiper/swiper.min.css";
 import "swiper/swiper-bundle.css";
 import "./assets/style/global.scss";
