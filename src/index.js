acfb1b1d5064925797034ad998db39a174d0c62f
Shahriddinov <shahriddinovmurod195@gmail.com>
2023-06-12 10:11:38 +0500

    fisrt

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./reduxToolkit/store";
 import Routes from "./routes";
+import i18 from "./services/i18n/i18n";
 import GrayContextProvider from "./context/GrayContext";
+import { Spinner } from "./component";
 
 import "swiper/swiper.min.css";
 import "swiper/swiper-bundle.css";
@@ -12,9 +14,6 @@ import "./assets/style/Footer.scss";
 import "./assets/style/Header.scss";
 import "animate.css/animate.min.css";
 import App from "./App";
-
-
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
