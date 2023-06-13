ab94872be16f3d33d98e0b6b17c7f41cbf344599
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2023-06-13 09:59:23 +0500

    commit

--- src/component/newsDetail/NewsDetail.jsx
+++ src/component/newsDetail/NewsDetail.jsx
@@ -11,7 +11,8 @@ import PortalLatestNews from "../PortalLatestNews/PortalLatestNews";
 import { PORTAL_IMAGE_URL } from "../../services/api/utils";
 import SiteLatestNews from "../siteLatestNews/SiteLatestNews";
 
-const SiteNewsDetail = ({ latestNews, detailData }) => {
+const SiteNewsDetail = ({ latestNews, detailData, type }) => {
+  console.log(type);
   return (
     <div className="news-detail">
       <div className="container">
@@ -31,7 +32,14 @@ const SiteNewsDetail = ({ latestNews, detailData }) => {
           <div className="news-detail-main">
             <div className="news-detail-main-desc">
               <div className="news-detail-main-desc-img">
-                <img src={`${PORTAL_IMAGE_URL}/${detailData?.image}`} alt="" />
+                <img
+                  src={`${PORTAL_IMAGE_URL}/${
+                    type === "event"
+                      ? JSON.parse(detailData?.image)[0]
+                      : detailData?.image
+                  }`}
+                  alt=""
+                />
               </div>
               <div className="news-detail-main-desc-action">
                 <div className="news-detail-main-desc-action-date-viewers">
@@ -44,14 +52,12 @@ const SiteNewsDetail = ({ latestNews, detailData }) => {
                     <span>{detailData?.view}</span>
                   </div>
                 </div>
-              
               </div>
               <div
                 className="news-detail-main-desc-texts"
                 dangerouslySetInnerHTML={{
                   __html: detailData.body,
-                }}
-              ></div>
+                }}></div>
               {/* {data?.images
         ? JSON.parse(data?.images).length >= 1 && (
             <div className="news-detail-main-desc-gallery">
