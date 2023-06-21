31114952800fe856403a00ad93b55a651aa55568
KhushnudEshtemirov <khushnudeshtemirov@gmail.com>
2023-06-21 09:09:57 +0500

    Updated

--- src/pages/Portal/volunter/pages/VolunterActivity/VolunterActivity.jsx
+++ src/pages/Portal/volunter/pages/VolunterActivity/VolunterActivity.jsx
@@ -8,9 +8,10 @@ import {
 } from "../../../../../reduxToolkit/newsSlice/extraReducer";
 import Aos from "aos";
 import { useState } from "react";
-import { Card, Hero, Pagination } from "../../../../../component";
+import { Card, Hero, Pagination, Spinner } from "../../../../../component";
 import { getPaginationCount } from "../../../../singlePage/extraFunck";
 import { useTranslation } from "react-i18next";
+import { useVolunteerHomeFetching } from "../VolunterHome/hooks/useVolunteerHomeFetching";
 
 export default function VolunterActivity() {
   const { t } = useTranslation();
@@ -21,14 +22,16 @@ export default function VolunterActivity() {
   const loading = useSelector((state) => state.sliderSlice.loading);
   const newsData = useSelector((state) => state.newsSlice.newsHomeData);
   const newsDataOne = useSelector((state) => state.newsSlice.newsData);
-  const errorcards = useSelector((state) => state.newsSlice.error);
   const paginationData = useSelector(
     (store) => store.singleSlice.paginationData
   );
+
+  const { volunteerNews, volunteerNewsLoading, volunteerNewsError } =
+    useVolunteerHomeFetching();
+
   useEffect(() => {
     dispatch(getSlider());
     dispatch(getHomeNews({ page: page }));
-    dispatch(getNews());
     setPage(1);
   }, [dispatch]);
 
@@ -36,20 +39,24 @@ export default function VolunterActivity() {
     Aos.init({ duration: 2000 });
   }, []);
 
-  if (error || errorcards) {
-    return <p>{error}</p>;
+  if (volunteerNewsLoading) {
+    return <Spinner position="full" />;
+  }
+
+  if (error || volunteerNewsError) {
+    return (
+      <p>
+        {error}
+        {volunteerNewsError}
+      </p>
+    );
   }
 
   const paginationFetching = (count) => {
     setPage(count);
-    dispatch(
-      getHomeNews({
-        page: count,
-      })
-    );
   };
 
-  const paginationCount = Math.ceil(newsDataOne?.length / 6);
+  const paginationCount = Math.ceil(volunteerNews?.data?.length / 6);
 
   return (
     <main className="volunteractivity">
@@ -57,12 +64,8 @@ export default function VolunterActivity() {
       <div className="container">
         <h4 className="volunteractivity-cardlist-title">{t("volunteryOne")}</h4>
         <div className="volunteractivity-cardlist">
-          {newsData?.map((news) => (
-            <Card
-              key={news.id}
-              {...news}
-              pathUrl="portal-category/volunteer/activity"
-            />
+          {volunteerNews?.data?.map((news) => (
+            <Card key={news.id} {...news} pathUrl="news" />
           ))}
         </div>
         <Pagination
